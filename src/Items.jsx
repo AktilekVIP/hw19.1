2f6d25fffe5be17b55946db1a0c24695629fa44e
Aktilek <janybaevaktilek@gmail.com>
2022-11-16 15:19:55 +0600

    commit

---
+++ src/Items.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const Items = ({ name, id, deleteId, editData }) => {
+  const [tast, setTask] = useState(name);
+  const [edit, setEdit] = useState(true);
+
+  
+  const changeEditContent = () => {
+    setEdit((prevState) => !prevState);
+  };
+
+  
+  return ( <div> {edit ? (
+        <>
+          {name}
+
+          <button onClick={() => deleteId(id)}>DELETE</button>
+          <button onClick={changeEditContent}>Edit</button>
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            value={tast}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button onClick={() => editData(id, tast,changeEditContent)}>save</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Items;
